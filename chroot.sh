8c131ea08e433530fa9ebe2868ec2a2bfc75c22d
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2015-03-23 10:33:41 +0530

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 	echo "chroot folder not specified"
 	exit 1
 elif [ "$1" = -h ]; then
-	echo "./chroot.sh <chroot-folder"
+	echo "./chroot.sh <chroot-folder>"
 	exit 1
 fi
 
@@ -14,22 +14,22 @@ location=$1
 cd $location
 
 # Mounting, basic
-sudo mount -t proc proc $1/proc/
-sudo mount -o bind /sys $1/sys/
-sudo mount -o bind /dev $1/dev/
+sudo mount -t proc proc $1/proc/ || exit 1
+sudo mount -o bind /sys $1/sys/ || exit 1
+sudo mount -o bind /dev $1/dev/ || exit 1
 
 # Mounting extra partition which is already mounted on host
 # first one is host mount path, second is chroot mount 
 sudo mount -B /mnt/datalinux2 $1/mnt/data
 
 # For internet access
-sudo cp /etc/resolv.conf etc/resolv.conf
+sudo cp /etc/resolv.conf $1/etc/resolv.conf
 
 # Finally, chroot
-sudo chroot $location /bin/bash
+sudo chroot $location /bin/bash || exit 1
 
 # Unmounting after exit from chroot
 sudo umount $1/mnt/data
-sudo umount proc/ sys/ dev/
+sudo umount $1/proc/ $1/sys/ $1/dev/ || exit 1
 
 echo "Done"
