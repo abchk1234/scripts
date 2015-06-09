bc73072aeaf956d73b40889bdcf435c4be4583e0
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2015-06-09 21:16:43 +0530

    geniso.sh: updated

--- arch-manjaro/geniso.sh
+++ arch-manjaro/geniso.sh
@@ -3,7 +3,7 @@
 # Inspired from https://forum.manjaro.org/index.php?topic=20637.msg186815#msg186815
 
 # Set defaults
-WORKDIR=/opt/buildiso
+WORKDIR=/var/lib/manjaro-tools/buildiso
 ARCH=$(uname -m)
 QUERY=false
 REPOS=()
@@ -13,6 +13,14 @@ if [[ $EUID -ne 0 ]]; then
 	echo "Must be run as root."
 	exit 1
 fi
+
+# Source for post_install tasks
+source /home/aaditya/mtools/post-install || exit 1
+
+# Colors
+BOLD="\e[1m"
+CLR="\e[0m"
+GREEN="\e[1;32m"
 
 # Get profile, arch and other options
 while getopts "p:a:b:r:wcxlisqh" opt
@@ -48,24 +56,54 @@ else
 	fi
 fi
 
+# Pre-install
+if [[ ! $QUERY = true ]]; then
+	rm -v /var/cache/pacman/pkg/tlp-pmu-*-any.pkg.tar.xz
+	rm -v /var/cache/pacman/pkg/openresolv-openrc-*-any.pkg.tar.xz
+fi
+
 # Build the profile chroot
-buildiso "$@" -i
+if [[ $@ != *-sc* ]] && [[ ! $QUERY = true ]]; then
+	buildiso "$@" -i
+fi
+
+echo -e "${GREEN}${BOLD}" "Performing custom tasks." "$CLR"
 
 # Remove extra repos from pacman.conf
-for repo in "${REPOS[@]}"
-do
+flag=0
+for repo in "${REPOS[@]}"; do
 	if ! echo "${VALID[*]}" | grep -q "$repo"; then
 		# Remove custom repo
 		for file in $WORKDIR/$PROFILE/$ARCH/{livecd,root,${PROFILE%-*}}-image/etc/pacman.conf
 		do
 			if [[ -f $file ]] && [[ ! $QUERY = true ]]; then
 				echo "Editing $file"
-				sed -i "/^\[$repo/,/^Server/d" "$file" || exit 1
+				sed -i "/^\[$repo/,/^Server/d" "$file" && flag=1 || exit 1
 			fi
 		done
-		echo "Custom repo $repo removed from pacman.conf"
+		if [[ $flag -eq 1 ]]; then
+			echo -e "${BOLD}${GREEN}" "Custom repo $repo removed from pacman.conf" "$CLR"
+		fi
 	fi
 done
+
+# Change branch to stable in pacman-mirrors.conf
+flag=0
+for file in $WORKDIR/$PROFILE/$ARCH/{livecd,root,${PROFILE%-*}}-image/etc/pacman-mirrors.conf; do
+	if [[ -f $file ]] && [[ ! $QUERY = true ]]; then
+		echo "Editing $file"
+		sed 's|Branch=testing|Branch=stable|' -i "$file" && flag=1 || exit 1
+	fi
+done
+if [[ $flag -eq 1 ]]; then
+	echo -e "${BOLD}${GREEN}" "Branch set to stable in pacman-mirrors.conf" "$CLR"
+fi
+
+# Perform post installation tasks
+if [[ ! $QUERY = true ]]; then
+	post_install
+	echo -e "${BOLD}${GREEN}" "Post-install tasks done." "$CLR"
+fi
 
 # Build iso
 buildiso "$@" -sc
