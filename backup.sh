7f40e2dc92a498065c5eba5df890e321a5506cad
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2015-06-14 19:52:16 +0530

    slightly update backup.sh

--- backup.sh
+++ backup.sh
@@ -30,10 +30,12 @@ case "$1" in
      ;;
 esac
 
-src=${2:-"/"}
+src=${2:-""}
 
 START=$(date +%s)
-rsync -aAXvC -H -h --delete "$src"/* "$1" --exclude={"$src"/dev/*,"$src"/proc/*,"$src"/sys/*,"$src"/tmp/*,"$src"/run/*,"$src"/mnt/*,"$src"/media/*,"$src"/lost+found,"$src"/var/lib/pacman/sync/*,"$src"/home/aaditya/DataLinux/*,"$src"/home/*/.thumbnails/*,"$src"/home/*/.mozilla/firefox/*.default/Cache/*,"$src"/home/aaditya/work/*,"$src"/home/aaditya/manjaroiso/*,"$src"/var/log/journal/*,"$src"/home/aaditya/src/kernel/*,"$src"/home/*/.cache/*,"$src"/root/.cache/*,"$src"/home/*/.gvfs,"$src"/*/.cache/*,"$src"/*/.thumbnails/*}
+
+rsync -aAXvC -H -h --delete "$src"/* "$1" --exclude={"$src/dev/"*,"$src/proc/"*,"$src/sys/"*,"$src/tmp/"*,"$src/run/"*,"$src/mnt/"*,"$src/media/"*,"$src/lost+found","$src/var/lib/pacman/sync/"*,"$src/home/aaditya/DataLinux/"*,"$src"/home/*/.thumbnails/*,"$src"/home/*/.mozilla/firefox/*.default/Cache/*,"$src/home/aaditya/manjaroiso/"*,"$src/var/log/journal/"*,"$src/home/aaditya/src/kernel/"*,"$src"/home/*/.cache/*,"$src/root/.cache/"*,"$src"/home/*/.gvfs,"$src"/*/.cache/*,"$src"/*/.thumbnails/*}
+
 FINISH=$(date +%s)
  
 echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds" | tee $1/backup/"Backup-from-$(date '+%A-%d-%B-%Y-%T')"
