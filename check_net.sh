aae19d52557126b78f5ad2ef5ab28923f0cbad8d
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-09-21 21:06:04 +0530

    updated and corrected check_net.sh

--- check_net.sh
+++ check_net.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # check_net: to check if internet connection is working or not
 
-# Counter
-min=0
+# counter
+i=0
+# sleep interval (seconds)
+int=5
 
 while true; do
-       	sleep 60
-	let min=$min+1
-	echo $min
-	ping google.com
-	if [ $? -eq 0 ]; then
-		notify-send "ping available"
-	fi
+        echo $i
+        ping -c 1 -W 3 -q google.com > /dev/null
+        if [ ! $? -eq 0 ]; then
+                notify-send -u low -t 2000 -a "check_net" "ping not available!"
+        fi
+        echo # line break
+        sleep $int
+        let i=$i+$int
 done
