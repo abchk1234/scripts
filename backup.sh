fa5e5d82fffa80e6a49e31420fa3dc7eef85b9d3
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-07-29 17:31:19 +0530

    script.sh: corrections

--- backup.sh
+++ backup.sh
@@ -36,12 +36,16 @@ START=$(date +%s)
 
 rsync -aAXv --delete-after ${src}/* $1 --exclude={${src}/dev/*,${src}/proc/*,${src}/sys/*,${src}/tmp/*,${src}/run/*,${src}/mnt/*,${src}/media/*,${src}/lost+found,${src}/swapfile,${src}/var/lib/pacman/sync/*,${src}/home/aaditya/DataLinux/*,${src}/home/*/.thumbnails/*,${src}/home/*/.mozilla/firefox/*.default/cache/*,${src}/home/*/.thunderbird/*.default/ImapMail/*,${src}/var/log/journal/*,${src}/home/aaditya/src/kernel/*,${src}/home/*/.cache/*,${src}/root/.cache/*,${src}/home/*/.gvfs,${src}/*/.cache/*,${src}/*/.thumbnails/*,${src}/root/.ccache/*,${src}/home/*/.local/share/Trash/*}
 
+status=$?
+
 FINISH=$(date +%s)
 
 # Reporting
 content_to_write="total time: $(( (FINISH - START) / 60 )) minutes, $(( (FINISH - START) % 60 )) seconds"
-mkdir -p "$1/backup"
-echo "$content_to_write" | tee "$1/backup/Backup-from-$(date '+%Y-%B-%d-%A-%T')"
-# same needs to be present in source as well or it will be deleted on next sync!
-mkdir -p "$/backup"
-echo "$content_to_write" | tee "$src/backup/Backup-from-$(date '+%Y-%B-%d-%A-%T')"
+if [ "$status" -eq 0 ]; then
+	mkdir -p "$1/backup"
+	echo "$content_to_write" | tee "$1/backup/Backup-from-$(date '+%Y-%B-%d-%A-%T')"
+	# same needs to be present in source as well or it will be deleted on next sync!
+	mkdir -p "$src/backup"
+	echo "$content_to_write" | tee "$src/backup/Backup-from-$(date '+%Y-%B-%d-%A-%T')"
+fi
